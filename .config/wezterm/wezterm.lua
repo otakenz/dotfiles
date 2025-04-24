3f66fb3cf02ae33fb1d6e1567184db7cc0fc3f6e
jhunhong <jhunhong.lee@outlook.com>
2025-04-24 12:51:52 +0800

    This setups is not very friendly to ubuntu 20..

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,6 +17,7 @@ config.font_size = 13
 --config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
 
 config.keys = {
