fc079ccd70ee735fd5de92124697fdf69ffd0b05
jhunhong <jhunhong.lee@outlook.com>
2025-05-06 03:16:34 +0800

    Add more keybindings to use multi cursor

--- .config/nvim/lua/plugins/multicursor.lua
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -6,6 +6,34 @@ return {
       local mc = require("multicursor-nvim")
       mc.setup()
 
+      -- Add or skip cursor above/below the main cursor.
+      MAP({ "n", "x" }, "<up>", function()
+        mc.lineAddCursor(-1)
+      end)
+      MAP({ "n", "x" }, "<down>", function()
+        mc.lineAddCursor(1)
+      end)
+      MAP({ "n", "x" }, "<leader><up>", function()
+        mc.lineSkipCursor(-1)
+      end)
+      MAP({ "n", "x" }, "<leader><down>", function()
+        mc.lineSkipCursor(1)
+      end)
+
+      -- Add or skip adding a new cursor by matching word/selection
+      MAP({ "n", "x" }, "<leader>j", function()
+        mc.matchAddCursor(1)
+      end)
+      MAP({ "n", "x" }, "<leader>k", function()
+        mc.matchSkipCursor(1)
+      end)
+      MAP({ "n", "x" }, "<leader>J", function()
+        mc.matchAddCursor(-1)
+      end)
+      MAP({ "n", "x" }, "<leader>K", function()
+        mc.matchSkipCursor(-1)
+      end)
+
       -- Add and remove cursors with CTRL + left click.
       MAP("n", "<c-leftmouse>", mc.handleMouse)
       MAP("n", "<c-leftdrag>", mc.handleMouseDrag)
@@ -14,6 +42,10 @@ return {
       -- Mappings defined in a keymap layer only apply when there are
       -- multiple cursors. This lets you have overlapping mappings.
       mc.addKeymapLayer(function(layerSet)
+        -- select a different cursor as the main one.
+        layerSet({ "n", "x" }, "<left>", mc.prevcursor)
+        layerSet({ "n", "x" }, "<right>", mc.nextcursor)
+
         -- Enable and clear cursors using escape.
         layerSet("n", "<esc>", function()
           if not mc.cursorsEnabled() then
