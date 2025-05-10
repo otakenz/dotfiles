a18cfb8621bbdb763dc79441644a4d91d9ab083e
jhunhong <jhunhong.lee@outlook.com>
2025-05-10 20:21:31 +0800

    Sanitize mason.lua to include explicitly lsp/dap/linter/formatter I use

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,11 +3,42 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        -- Shell
+        "bash-language-server",
         "shellcheck",
         "shfmt",
-        "stylua",
+        -- Clang
+        "clang-format",
         "clangd",
-        "cmakelint",
+        "codelldb",
+        -- CMake
+        "cmakelang",
+        "neocmakelsp",
+        -- Docker
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "hadolint",
+        -- Go
+        "delve",
+        "gofumpt",
+        "goimports",
+        "golangci-lint",
+        "gopls",
+        -- Lua
+        "lua-language-server",
+        "stylua",
+        -- Python
+        "debugpy",
+        "pyright",
+        "ruff",
+        -- Json, Markdown, Yaml, Toml
+        "json-lsp",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "taplo",
+        "yaml-language-server",
+        -- Rust
         "rust-analyzer",
       },
     },
