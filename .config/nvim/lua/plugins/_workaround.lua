00a3de796710d5c14d1de04e1ae4f11240c0dd77
jhunhong <jhunhong.lee@outlook.com>
2025-06-14 22:36:37 +0800

    chore: Update packages

--- .config/nvim/lua/plugins/_workaround.lua
+++ .config/nvim/lua/plugins/_workaround.lua
@@ -1,5 +1,8 @@
 return {
 	-- ISSUE: https://github.com/Saghen/blink.cmp/issues/1727#issuecomment-2867452782
+	-- Remember to manually build rust implementation of blink.cmp when needed.
+	-- rust@nightly required to build
+	-- cargo build --release (at ~/.local/share/nvim/lazy/blink.cmp)
 	{
 		"saghen/blink.cmp",
 		opts = {
