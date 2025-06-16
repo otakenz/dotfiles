0b1eed7cdf277bdcfa62570507c38576dc530044
jhunhong <jhunhong.lee@outlook.com>
2025-06-16 23:25:05 +0800

    chore: Remove workaround for blink.cmp
    - blink.cmp v1.4.0 introduces fix for large completion for fuzzy.rs,
    therefore workaround is removed
    - Update packages

--- .config/nvim/lua/plugins/_workaround.lua
+++ .config/nvim/lua/plugins/_workaround.lua
@@ -1,11 +1,3 @@
 return {
-	-- ISSUE: https://github.com/Saghen/blink.cmp/pull/1908
-	-- PR is merged in the main branch of blink.cmp, but not yet in tagged release.
-	{
-		"Saghen/blink.cmp",
-		branch = "main",
-		version = false,
-		-- rust@nightly required to build the library as no pre-built binaries are available.
-		build = "cargo build --release",
-	},
+	-- {},
 }
