90eb4c5d980a68d13c75d3345b5736c285b80005
jhunhong <jhunhong.lee@outlook.com>
2026-03-14 14:10:29 +0800

    fix: snacks.nvim workaround

--- .config/nvim/lua/plugins/_workaround.lua
+++ .config/nvim/lua/plugins/_workaround.lua
@@ -1,3 +1,9 @@
 return {
-	-- {},
+	-- https://github.com/folke/snacks.nvim/pull/2768
+	-- Pull from fork branch until the fix is merged upstream; remove this entry then.
+	{
+		"folke/snacks.nvim",
+		url = "https://github.com/otakenz/snacks.nvim.git",
+		branch = "fix/grep-file-sep-nil-guard",
+	},
 }
